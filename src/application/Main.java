898c504cd396575fa7e9865a7cfe3026119cc71d
nassim tchou <gn_tchoulak@esi.dz>
2019-12-09 19:35:33 +0100

    sol2

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,36 +1,4 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
-
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			//BorderPane root = new BorderPane();
-			//Scene scene = new Scene(root,400,400);
-			
-			Parent root = (Parent) FXMLLoader.load(getClass().getResource("home.fxml"));
-	           // AnchorPane root = loader.load();
-	           // maincontroller mainWindowController = loader.getController();
-	           // mainWindowController.setMain(this);
-	            
-	            
-				Scene scene = new Scene(root);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+public class Main {
 }
